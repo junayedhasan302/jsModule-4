476119f6eff6e5e52374e7ab2aceff1dc53c0a5c
junayedhasan302 <junayedhasan302@gmail.com>
2026-08-01 00:37:37 +0600

    Nothing

--- advanced3.js
+++ advanced3.js
@@ -1,5 +1,5 @@
 // 3. Given an array of numbers, find the largest and smallest value without using Math.max or Math.min directly on the whole array (use a loop).
-
+// JUNAYED HASAN
 let numbers = [20, 50, 70, 10, 30, 55, 60, 90, 80];
 
 let largest = numbers[0];
